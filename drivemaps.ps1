1e1d847da8d0a548b7f79a698b50560903292b6a
dave-hernandez <dhernandez@PNC-DJH2.proactiveway.local>
2015-08-11 12:28:10 -0700

    First Push
    Sanitized to protect sensitive data. Users MUST modify the first line
    -SearchBase to reflect their environment

---
+++ drivemaps.ps1
@@ -0,0 +1,19 @@
+﻿Get-ADComputer -Filter * -SearchBase "OU=MY OU,DC=MYDOMAIN,DC=COM" | fl -Property Name > C:\raw.txt
+Get-Content C:\raw.txt | ForEach-Object {$_ -replace 'Name : ',''} | Set-Content "C:\raw2.txt"
+
+Select-String -Pattern "\w" -Path C:\raw2.txt |
+    ForEach-Object { $_.line } |
+    Set-Content C:\adcomputers.txt
+
+Remove-Item C:\raw.txt
+Remove-Item C:\raw2.txt
+
+$computerlist = Get-Content C:\adcomputers.txt
+    
+    ForEach ($computer in $computerlist)
+        
+        {
+
+           Get-WmiObject Win32_MappedLogicalDisk -ComputerName $Computer | Select name, providername | ft
+        
+        }
